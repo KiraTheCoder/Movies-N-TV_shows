5ed8811fe3c7c9a081ec44582ab05685c4e8072f
KiraTheCoder <kiranbohraofficial@gmail.com>
2023-09-12 23:14:44 +0530

    remove classes

--- js/Componets/Fetch.js
+++ js/Componets/Fetch.js
@@ -9,36 +9,36 @@ function hideSpinner() {
 }
 
 // FUN for Fetch data from TMDB API
-class Fetch {
-  static async fetchData(endPoint) {
-    const API_KEY = global.api.apiKey;
-    const API_URL = global.api.apiUrl;
 
-    showSpinner();
-    const response = await fetch(
-      `${API_URL}${endPoint}?api_key=${API_KEY}&language=en-US&page=${global.pages.current}`
-    );
+async function fetchData(endPoint) {
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
 
-    const data = await response.json();
-    hideSpinner();
+  showSpinner();
+  const response = await fetch(
+    `${API_URL}${endPoint}?api_key=${API_KEY}&language=en-US&page=${global.pages.current}`
+  );
 
-    return data;
-  }
+  const data = await response.json();
+  hideSpinner();
 
-  //  Make request to Search
-  static async searchAPIData() {
-    const API_KEY = global.api.apiKey;
-    const API_URL = global.api.apiUrl;
-
-    showSpinner();
-    const response = await fetch(
-      `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`
-    );
-
-    const data = await response.json();
-    hideSpinner();
-
-    return data;
-  }
+  return data;
 }
-export { Fetch.fetchData, Fetch.searchAPIData };
+
+//  Make request to Search
+async function searchAPIData() {
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
+
+  showSpinner();
+  const response = await fetch(
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`
+  );
+
+  const data = await response.json();
+  hideSpinner();
+
+  return data;
+}
+
+export { fetchData, searchAPIData };
